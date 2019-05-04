011d8bf544f49e19c27526725c4a3955eb244e3b
farside1969 <farside1969@hotmail.com>
2019-05-04 14:34:15 -0500

    intial 4.1 corrections

--- main.py
+++ main.py
@@ -3,10 +3,41 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+<form>
+    <!DOCTYPE html>
 
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+            <!-- create your form here -->
+            <form method="post">
+                <label for="rotate_by">Rotate by:</label>
+                    <input type="text" name="rot" value ="0"/>
+                    <textarea name="text" rows="5" cols="20"/>
+                    <input type="submit"/>
+            </form>
+        </body>
+    </html>
+</form>
 
 @app.route("/")
 def index():
-    return render_template("form.html")
+    return "Hello World"
 
 app.run()
