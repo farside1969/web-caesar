6db379a44a72332e4c884a4a5c59c8c76574e903
farside1969 <farside1969@hotmail.com>
2019-05-04 15:36:23 -0500

    completed form pre-encryption

--- main.py
+++ main.py
@@ -1,43 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-<form>
-    <!DOCTYPE html>
-
-    <html>
-        <head>
-            <style>
-                form {
-                    background-color: #eee;
-                    padding: 20px;
-                    margin: 0 auto;
-                    width: 540px;
-                    font: 16px sans-serif;
-                    border-radius: 10px;
-                }
-                textarea {
-                    margin: 10px 0;
-                    width: 540px;
-                    height: 120px;
-                }
-            </style>
-        </head>
-        <body>
-            <!-- create your form here -->
-            <form method="post">
-                <label for="rotate_by">Rotate by:</label>
-                    <input type="text" name="rot" value ="0"/>
-                    <textarea name="text" rows="5" cols="20"/>
-                    <input type="submit"/>
-            </form>
-        </body>
-    </html>
-</form>
-
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("form.html")
 
 app.run()
