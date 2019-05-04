fa9d7c9f65dbe7243f1c128c4a0b71bd7b7c1263
farside1969 <farside1969@hotmail.com>
2019-05-04 15:56:30 -0500

    completed form processing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, request, redirect
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -6,5 +7,13 @@ app.config['DEBUG'] = True
 @app.route("/")
 def index():
     return render_template("form.html")
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = int(request.form['rot'])
+    text = request.form['text']
+    encrypted_text = rotate_string(text, rot)
+    return '<h1>' + encrypted_text + '</h1>'
+
 
 app.run()
